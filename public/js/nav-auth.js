471aad8458f696a097c2914cdab5db3c9bd94556
Ryan <rhazzardphoto@gmail.com>
2020-04-22 16:53:39 -0700

    3

--- public/js/nav-auth.js
+++ public/js/nav-auth.js
@@ -86,7 +86,6 @@ if (authHelper.isLoggedIn()) {
       </div>
       `);
     } else if (userInfo && userInfo.isProfessionalUser) {
-      $('.nav-spacer').css('display', 'none')
       $("#register-column").html(`
       <div id="client-drop-div">
         <div id="client-dropdown" class="ui inline dropdown">
@@ -111,7 +110,6 @@ if (authHelper.isLoggedIn()) {
       $(".ui.dropdown").dropdown({ transition: "drop" });
       $("#sign-in-column").html("");
     } else if (userInfo && userInfo.isAdminUser) {
-      $('.nav-spacer').css('display', 'none')
       $("#register-column").html(`
       <div id="client-drop-div">
         <div id="client-dropdown" class="ui inline dropdown">
