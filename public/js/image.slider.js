aa1bf78db279c4c3466f5abf3347ef7c6141afab
Ryan <rhazzardphoto@gmail.com>
2020-06-02 14:24:52 -0700

    mac

--- public/js/image.slider.js
+++ public/js/image.slider.js
@@ -17,8 +17,8 @@ $(document).ready(setTimeout(function () {
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 2,
+              slidesToScroll: 1,
               infinite: true,
             }
           },
@@ -26,7 +26,7 @@ $(document).ready(setTimeout(function () {
             breakpoint: 750,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToScroll: 1
             }
           },
           {
